44e1eebf21c5e00d9a3fc3fbdd7012853e7b3853
unknown <otaviosouzalu@gmail.com>
2023-04-10 14:15:06 -0300

    novo desafio knect load

---
+++ 22-Kinect Load/script.js
@@ -0,0 +1,21 @@
+const t1 = document.querySelector('.t1');
+const t2 = document.querySelector('.t2');
+
+
+
+let cont = 90;
+
+  setInterval(rotate,1000)
+   setInterval(rotate2,1000)
+   
+    function rotate(){
+
+   
+}
+
+
+function rotate2(){
+
+
+}
+
